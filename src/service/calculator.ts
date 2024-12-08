96a792d7f12fa845203473e62a960eaffebf4661
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2024-12-09 09:58:01 +1100

    update calculator.ts with data constructor

--- src/service/calculator.ts
+++ src/service/calculator.ts
@@ -3,4 +3,8 @@ import { Account } from '../types/account';
 export class AccountingCalculator {
     private data: Account[];
 
+    constructor(data: Account[]) {
+        this.data = data;
+    }
+
 }
